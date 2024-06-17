9cb7f45993036ec5beeee7c1d2cc8f89a703c2e9
Harshal Kumre <kumreharshalkumar@gmail.com>
2024-06-17 17:17:08 +0530

    code updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "shipping-price-prediction",
+    version = "0.0.1",
+    author = "Harshal Kumrer",
+    author_email = "kumreharshalkumar@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+)
